11f7770a0332210ae89a814f0c646a27bdc4a4d3
cyberkidsday <donotreply@github.com>
2018-07-15 23:31:41 +1000

    .

--- gompromise.rb
+++ gompromise.rb
@@ -4,11 +4,7 @@ class Gompromise < Formula
   url "https://github.com/threatact0r/gompromise/blob/master/bin/gompromise-1.0.0.tar.gz"
   sha256 "2703e953789349d102e1df8e7e0f85066cf555f9f209d570b917ff0bd40ae831"
   version "1.0.0"
-
-  depends_on "curl"
-
   bottle :unneeded
-
   def install
     bin.install "gompromise"
   end
