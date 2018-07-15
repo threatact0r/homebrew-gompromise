3c68e9b3d1e5a9f4b297f9f729d5575f55699282
cyberkidsday <donotreply@github.com>
2018-07-15 23:35:08 +1000

    .

--- gompromise.rb
+++ gompromise.rb
@@ -2,7 +2,7 @@ class Gompromise < Formula
   desc "A command line tool to showcase brew supply chain compromise"
   homepage "https://github.com/threatact0r/gompromise"
   url "https://github.com/threatact0r/gompromise/blob/master/bin/gompromise-1.0.0.tar.gz"
-  sha256 "2703e953789349d102e1df8e7e0f85066cf555f9f209d570b917ff0bd40ae831"
+  sha256 "7ac06b30088775593a43028886a2f112ce404c0e737f39708b1597e6e5aa9524"
   version "1.0.0"
   bottle :unneeded
   def install
